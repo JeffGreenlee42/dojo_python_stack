e0b15065b8687f17bd0bb8cf1049053f4a97494b
JeffGreenlee42 <banjo1@gmail.com>
2023-03-17 20:19:35 -0700

    Working on problem 2 of nest_dictionaries_and_lists but not done yet with assignment

---
+++ nested_dictionaries_and_lists/nested.py
@@ -0,0 +1,61 @@
+# 1) Update Values in Dictionaries and Lists
+# Change the value 10 in x to 15. Once you're done, x should now be [ [5,2,3], [15,8,9] ].
+# Change the last_name of the first student from 'Jordan' to 'Bryant'
+# In the sports_directory, change 'Messi' to 'Andres'
+# Change the value 20 in z to 30
+
+x = [ [5,2,3], [10,8,9] ] 
+students = [
+     {'first_name':  'Michael', 'last_name' : 'Jordan'},
+     {'first_name' : 'John', 'last_name' : 'Rosales'}
+]
+sports_directory = {
+    'basketball' : ['Kobe', 'Jordan', 'James', 'Curry'],
+    'soccer' : ['Messi', 'Ronaldo', 'Rooney']
+}
+z = [ {'x': 10, 'y': 20} ]
+
+x[1][0] = 15
+print(x)
+
+students[0]["last_name"] = "Bryant"
+print(students)
+
+sports_directory["soccer"][0] = "Andre"
+print(sports_directory)
+
+z[0]['y'] = 30
+print(z)
+
+# 2) Iterate Through a List of Dictionaries
+# Create a function iterateDictionary(some_list) that, 
+# given a list of dictionaries, 
+# the function loops through each dictionary in the list and prints each key and the associated value. For example, given the following list:
+
+students = [
+         {'first_name':  'Michael', 'last_name' : 'Jordan'},
+         {'first_name' : 'John', 'last_name' : 'Rosales'},
+         {'first_name' : 'Mark', 'last_name' : 'Guillen'},
+         {'first_name' : 'KB', 'last_name' : 'Tonel'}
+    ]
+
+def iterateDictionary(aList):
+    for item in students:
+        printLine = ""
+        for key, val in item.items():
+            if printLine.__contains__(','):
+                printLine = printLine + f"{key}) - {val}"
+            else:
+                printLine = f"{key} - {val}" + ","
+
+            print(printLine)
+
+iterateDictionary(students) 
+# should output: (it's okay if each key-value pair ends up on 2 separate lines;
+# bonus to get them to appear exactly as below!)
+# first_name - Michael, last_name - Jordan
+# first_name - John, last_name - Rosales
+# first_name - Mark, last_name - Guillen
+# first_name - KB, last_name - Tonel
+
+
