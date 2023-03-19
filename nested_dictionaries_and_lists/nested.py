2563c3bb86b053e88e33bf6704314f7c208e4670
JeffGreenlee42 <banjo1@gmail.com>
2023-03-18 18:22:52 -0700

    Finished assignenment 'nested_dictionaries_and_liss'

--- nested_dictionaries_and_lists/nested.py
+++ nested_dictionaries_and_lists/nested.py
@@ -30,7 +30,8 @@ print(z)
 # 2) Iterate Through a List of Dictionaries
 # Create a function iterateDictionary(some_list) that, 
 # given a list of dictionaries, 
-# the function loops through each dictionary in the list and prints each key and the associated value. For example, given the following list:
+# the function loops through each dictionary in the list and prints each key and 
+# the associated value. For example, given the following list:
 
 students = [
          {'first_name':  'Michael', 'last_name' : 'Jordan'},
@@ -40,17 +41,17 @@ students = [
     ]
 
 def iterateDictionary(aList):
-    for item in students:
+    for item in aList:
         printLine = ""
         for key, val in item.items():
-            if printLine.__contains__(','):
-                printLine = printLine + f"{key}) - {val}"
+            if len(printLine) == 0:
+                printLine = f"{key} - {val}, "
             else:
-                printLine = f"{key} - {val}" + ","
-
-            print(printLine)
+                printLine =  printLine +  f"{key} - {val}"
+        print(printLine)
 
 iterateDictionary(students) 
+
 # should output: (it's okay if each key-value pair ends up on 2 separate lines;
 # bonus to get them to appear exactly as below!)
 # first_name - Michael, last_name - Jordan
@@ -58,4 +59,45 @@ iterateDictionary(students)
 # first_name - Mark, last_name - Guillen
 # first_name - KB, last_name - Tonel
 
+# 3) . Get Values From a List of Dictionaries
+# Create a function iterateDictionary2(key_name, some_list) that,
+# given a list of dictionaries and a key name,
+# the function prints the value stored in that key for each dictionary.
+# For example, iterateDictionary2('first_name', students) should output:
+# Michael
+# John
+# Mark
+# KB
 
+# And iterateDictionary2('last_name', students) should output:
+# Jordan
+# Rosales
+# Guillen
+# Tonel
+
+def iterateDictionary2(key_name, some_list):
+     for item in some_list:
+          print(item[key_name])
+
+iterateDictionary2("first_name", students)
+iterateDictionary2("last_name", students)
+
+
+# 4.  Iterate Through a Dictionary with List Values
+# Create a function printInfo(some_dict) that given a dictionary whose values are all lists,
+# prints the name of each key along with the size of its list,
+# and then prints the associated values within each key's list.
+
+dojo = {
+   'locations': ['San Jose', 'Seattle', 'Dallas', 'Chicago', 'Tulsa', 'DC', 'Burbank'],
+   'instructors': ['Michael', 'Amy', 'Eduardo', 'Josh', 'Graham', 'Patrick', 'Minh', 'Devon']
+}
+
+def printInfo(some_dict):
+    for key,val in some_dict.items():
+            print(f"{len(val)} {key.upper()}")
+            for listVal in val:
+                print (listVal)
+            print("\n")
+
+printInfo(dojo)
