ec90ca5732b32fe5b9465a73fc33faa62f44d3b9
Jeff Greenlee <banjo1@gmail.com>
2023-04-11 03:08:01 -0700

    Finishing up Sakila practice assignment

---
+++ ERD/Sakila.sql
@@ -0,0 +1,45 @@
+USE sakila;
+
+
+SELECT * FROM customer;
+SELECT * from city;
+
+-- SELECT * What query would you run to get all the customers inside city_id = 312?
+-- Your query should return customer first name, last name, email, and address.
+
+SELECT CONCAT(customer.first_name, " ", customer.last_name) AS Name, customer.email AS "Email",
+CONCAT(address.address, " ", address.address2, " ", city.city, " ", address.postal_code) AS "Address"
+FROM customer
+JOIN address ON customer.address_id = address.address_id
+JOIN city ON address.city_id = city.city_id
+WHERE city.city_id = 312;
+
+-- What query would you run to get all comedy films?
+-- Your query should return film title, description, release year, rating, special features, and genre (category).
+
+SELECT film.title AS "Title", film.description "Description", film.release_year AS "Release Year",
+film.rating AS "Rating", film.special_features AS "Special Features", category.name AS "Genre"
+FROM film
+JOIN film_category ON film.film_id = film_category.film_id
+JOIN category ON film_category.category_id = category.category_id
+WHERE category.name = "Comedy";
+
+-- What query would you run to get all the films joined by actor_id=5?
+-- Your query should return the actor id, actor name, film title, description, and release year.
+
+SELECT actor.actor_id AS "Actor ID", CONCAT(actor.first_name, " ", actor.last_name) AS "Name",
+film.title AS "Film", film.description AS "Description", film.release_year AS "Release Year"
+FROM film
+JOIN film_actor ON film.film_id = film_actor.film_id
+JOIN actor ON film_actor.actor_id = actor.actor_id
+WHERE actor.actor_id = 5;
+
+-- What query would you run to get all the customers in store_id = 1 and inside these cities (1, 42, 312 and 459)?
+-- Your query should return customer first name, last name, email, and address.
+
+SELECT * FROM store
+JOIN address ON store.address_id = address.address_id
+JOIN city ON address.city_id = city.city_id
+WHERE address.city_id IN (1, 42, 312, 459
+
+SELECT * from store;
