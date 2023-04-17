c34bd7454f2f0a8cb83126d22d0ed3b7dde92813
Jeff Greenlee <banjo1@gmail.com>
2023-04-16 20:14:07 -0700

    added Checkerboard assignment

--- Flask/Checkerboard/server.py
+++ Flask/Checkerboard/server.py
@@ -2,16 +2,26 @@ from flask import Flask, render_template
 
 app = Flask(__name__)
 
-@app.route('/')
-def index():
-    return "Hello! This is not checkerboard!  Try again?"
-
-
 # level 1:
-@app.route('/checkerboard')
-def checkerboardy():
+@app.route('/')
+def full_checkerboard():
     return render_template("checkerboard.html")
 
+@app.route('/4')
+def checkerboard_4by8():
+    return render_template("checkerboard_4by8.html")
+
+@app.route('/<int:x>')
+def checkerboard_x(x):
+    return render_template("checkerboard_x.html", x=x)
+
+@app.route('/<int:x>/<int:y>')
+def checkerboard_xy(x, y):
+    return render_template("checkerboard_xy.html", x=x, y=y)
+
+@app.route('/<int:x>/<int:y>/<string:odd_color>/<string:even_color>')
+def checkerboard_xy_color(x, y, odd_color, even_color):
+    return render_template("checkerboard_xy_color.html", x=x, y=y, odd_color=odd_color, even_color=even_color)
 
 
 if __name__ == "__main__":
