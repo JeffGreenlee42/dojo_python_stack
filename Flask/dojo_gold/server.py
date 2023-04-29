0b0111218006d2cc8fc9db94edcc794df0d3c776
Jeff Greenlee <banjo1@gmail.com>
2023-04-29 00:31:35 -0700

    nearing completion of Dojo Gold  on Sensei challenges

--- Flask/dojo_gold/server.py
+++ Flask/dojo_gold/server.py
@@ -4,6 +4,11 @@ import random
 app = Flask(__name__)
 app.secret_key = 'keep it secret, keep it safe'
 
+# This Boolean records whether each transactgion is a net gain or loss.
+# It will be used in Tuple "Activities" stored in Session list
+GAIN = True
+LOSS = False
+
 @app.route('/')
 def index():
     if 'gold' not in session:
@@ -15,33 +20,35 @@ def index():
 @app.route('/process_money', methods=['POST'])
 def process_money():
     print(request.form)
+    print(session["activities"])
     if request.form['process_money'] == 'farm':
         gold = random.randint(10, 20)
         session['gold'] += gold
-        session['activities'].append(f"The Farm increased your gold by {gold} Gold pieces!" )
+        session['activities'].append((f"The Farm increased your gold by {gold} Gold pieces!", GAIN) )
         return redirect('/')
 
     if request.form['process_money'] == 'cave':
         gold = random.randint(5, 10)
         session['gold'] += gold
-        session['activities'].append(f"The Farm increased your gold by {gold} Gold pieces!" )
+        session['activities'].append((f"The Cave increased your gold by {gold} Gold pieces!", GAIN) )
         return redirect('/')
 
     if request.form['process_money'] == 'house':
         gold = random.randint(2, 5)
         session['gold'] += gold
-        session['activities'].append(f"The Farm increased your gold by {gold} Gold pieces!" )
+        session['activities'].append((f"The House increased your gold by {gold} Gold pieces!", GAIN) )
         return redirect('/')
 
     if request.form['process_money'] == 'casino':
         gold = random.randint(-50, 50)
+        is_richer = GAIN
         session['gold'] += gold
         if gold >= 1:
             profit = "increased"
         else:
             profit = "decreased"
-
-        session['activities'].append(f"The Farm {profit} your gold by {gold} Gold pieces!" )
+            is_richer = LOSS
+        session['activities'].append((f"The Casino {profit} your gold by {gold} Gold pieces!", is_richer) )
         return redirect('/')
 
 @app.route("/clear")
