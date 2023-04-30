133400997f8610c26b2144762e54ecf54e4825c8
JeffGreenlee42 <banjo1@gmail.com>
2023-04-29 21:57:18 -0700

    Completed Ninja Goldgit status! complete with all Sensei challenges

--- Flask/dojo_gold/server.py
+++ Flask/dojo_gold/server.py
@@ -14,42 +14,51 @@ def index():
     if 'gold' not in session:
         session['gold'] = 0;
         session['activities'] = []
+        session['turns_left'] = 15
     print(f"Your gold amount is: {session['gold']}")
     return render_template('gold.html')
 
-@app.route('/process_money', methods=['POST'])
-def process_money():
+@app.route('/process_money/<min_amt>/<max_amt>', methods=['POST'])
+def process_money(min_amt, max_amt):
+    session['turns_left'] -= 1
     print(request.form)
     print(session["activities"])
-    if request.form['process_money'] == 'farm':
-        gold = random.randint(10, 20)
-        session['gold'] += gold
-        session['activities'].append((f"The Farm increased your gold by {gold} Gold pieces!", GAIN) )
-        return redirect('/')
+    gold = random.randint(int(min_amt), int(max_amt))
+    is_richer = GAIN
+    session['gold'] += gold
+    if gold >= 1:
+        profit = "increased"
+    else:
+        profit = "decreased"
+        is_richer = LOSS
+    session['activities'].append((f"The {request.form['process_money']} {profit} your gold by {gold} Gold pieces!", is_richer) )
+    return redirect('/')
 
-    if request.form['process_money'] == 'cave':
-        gold = random.randint(5, 10)
-        session['gold'] += gold
-        session['activities'].append((f"The Cave increased your gold by {gold} Gold pieces!", GAIN) )
-        return redirect('/')
+# Below is the original solution, which I superseded by the last Sensei challange!
 
-    if request.form['process_money'] == 'house':
-        gold = random.randint(2, 5)
-        session['gold'] += gold
-        session['activities'].append((f"The House increased your gold by {gold} Gold pieces!", GAIN) )
-        return redirect('/')
+    # if request.form['process_money'] == 'cave':
+    #     gold = random.randint(5, 10)
+    #     session['gold'] += gold
+    #     session['activities'].append((f"The Cave increased your gold by {gold} Gold pieces!", GAIN) )
+    #     return redirect('/')
 
-    if request.form['process_money'] == 'casino':
-        gold = random.randint(-50, 50)
-        is_richer = GAIN
-        session['gold'] += gold
-        if gold >= 1:
-            profit = "increased"
-        else:
-            profit = "decreased"
-            is_richer = LOSS
-        session['activities'].append((f"The Casino {profit} your gold by {gold} Gold pieces!", is_richer) )
-        return redirect('/')
+    # if request.form['process_money'] == 'house':
+    #     gold = random.randint(2, 5)
+    #     session['gold'] += gold
+    #     session['activities'].append((f"The House increased your gold by {gold} Gold pieces!", GAIN) )
+    #     return redirect('/')
+
+    # if request.form['process_money'] == 'casino':
+    #     gold = random.randint(-50, 50)
+    #     is_richer = GAIN
+    #     session['gold'] += gold
+    #     if gold >= 1:
+    #         profit = "increased"
+    #     else:
+    #         profit = "decreased"
+    #         is_richer = LOSS
+    #     session['activities'].append((f"The Casino {profit} your gold by {gold} Gold pieces!", is_richer) )
+    #     return redirect('/')
 
 @app.route("/clear")
 def clear():
